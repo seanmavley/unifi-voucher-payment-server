e56045ebea4c7674ef75c99f0b1ec068a6449236
Rexford Nkansah <seanmavley@gmail.com>
2017-09-27 21:27:20 +0000

    updated .config_sample.js

--- .config_sample.js
+++ .config_sample.js
@@ -6,4 +6,15 @@ module.exports = {
   'account_id': 'get from hubtel',
   'database': 'mongodb://localhost/useyourown',
   'test': 'mongodb://localhost/useyourown',
+  // Please, it is 2017, use HTTPS for your unifi controller
+  // https://blog.khophi.co/use-letsencrypt-unifi-controller-ubuntu/
+  'baseUrl': 'https://your-unifi-controller:8443', // The URL of the Unifi Controller
+  'username': 'your-unifi-controller-username',
+  'password': 'your-unifi-controller-password',
+  // debug: true, // More debug of the API (uses the debug module)
+  // debugNet: true // Debug of the network requests (uses request module)
+
+  // Your ReCAPTCHA API credentials
+  'sitekey': 'something-something',
+  'sitesecret': 'duh-duh'
 }
